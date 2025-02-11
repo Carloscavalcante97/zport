6f1211be77746dc6358e434ed9409a589bca5d32
Antônio Carlos Bezerra Cavalcante Junior <126694183+Carloscavalcante97@users.noreply.github.com>
2025-02-10 23:40:31 -0300

    feito

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,22 +3,41 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col gap-1">
-      <Link href="mLinkilto:carlos.developer@icloud.com">
-        <Image alt="Logo Mail" src="/email.svg" width={50} height={50} />
-      </Link>
-      <Link href={"https://www.linkedin.com/in/antonio-dev-/"}>
+    <footer className="flex flex-col items-center gap-4 p-4 ">
+      {/* Ícone do E-mail */}
+      <a href="mailto:carlos.developer@icloud.com" target="_blank">
         <Image
-          alt="Logo LinkedIn"
-          src={"/linkedin.svg"}
+          alt="Logo Mail"
+          src="/email.svg"
           width={50}
           height={50}
+          className="w-12 h-12 hover:scale-110 transition-transform"
+        />
+      </a>
+
+      {/* Ícone do LinkedIn */}
+      <Link href="https://www.linkedin.com/in/antonio-dev-/" target="_blank">
+        <Image
+          alt="Logo LinkedIn"
+          src="/linkedin.svg"
+          width={50}
+          height={50}
+          className="w-12 h-12 hover:scale-110 transition-transform"
         />
       </Link>
-      <Link href={"https://github.com/Carloscavalcante97"}>
-        <Image alt="Logo Github" src={"/github.svg"} width={50} height={50} />
+
+      {/* Ícone do Github */}
+      <Link href="https://github.com/Carloscavalcante97" target="_blank">
+        <Image
+          alt="Logo Github"
+          src="/github.svg"
+          width={50}
+          height={50}
+          className="w-12 h-12 hover:scale-110 transition-transform"
+        />
       </Link>
     </footer>
   );
 };
+
 export default Footer;
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -3,8 +3,6 @@ import RepositoryCard from "../components/repositoryCard";
 
 export default function Projects() {
     return( 
-
-        
         <div className="min-h-screen bg-gray-950  text-white">
         <RepositoryCard />
     </div>
