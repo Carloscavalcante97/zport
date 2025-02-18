66ab59f2781376eff4b428435acf232889db9342
Antônio Carlos Bezerra Cavalcante Junior <126694183+Carloscavalcante97@users.noreply.github.com>
2025-02-18 18:20:49 -0300

    (FEAT): Deixando responsivo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,39 +3,41 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col items-center gap-4 p-4 ">
-      {/* Ícone do E-mail */}
-      <a href="mailto:carlos.developer@icloud.com" target="_blank">
-        <Image
-          alt="Logo Mail"
-          src="/email.svg"
-          width={50}
-          height={50}
-          className="w-12 h-12 hover:scale-110 transition-transform"
-        />
-      </a>
+    <footer className="fixed bottom-0 left-0 w-full bg-gray-950 text-white p-4 flex flex-row justify-center sm:justify-between items-center sm:px-10 shadow-md z-50 flex-wrap">
+      <div className="flex gap-6 flex-wrap justify-center sm:justify-start">
+        {/* Ícone do E-mail */}
+        <a href="mailto:carlos.developer@icloud.com" target="_blank">
+          <Image
+            alt="Logo Mail"
+            src="/email.svg"
+            width={50}
+            height={50}
+            className="w-10 h-10 sm:w-12 sm:h-12 hover:scale-110 transition-transform"
+          />
+        </a>
 
-      {/* Ícone do LinkedIn */}
-      <Link href="https://www.linkedin.com/in/antonio-dev-/" target="_blank">
-        <Image
-          alt="Logo LinkedIn"
-          src="/linkedin.svg"
-          width={50}
-          height={50}
-          className="w-12 h-12 hover:scale-110 transition-transform"
-        />
-      </Link>
+        {/* Ícone do LinkedIn */}
+        <Link href="https://www.linkedin.com/in/antonio-dev-/" target="_blank">
+          <Image
+            alt="Logo LinkedIn"
+            src="/linkedin.svg"
+            width={50}
+            height={50}
+            className="w-10 h-10 sm:w-12 sm:h-12 hover:scale-110 transition-transform"
+          />
+        </Link>
 
-      {/* Ícone do Github */}
-      <Link href="https://github.com/Carloscavalcante97" target="_blank">
-        <Image
-          alt="Logo Github"
-          src="/github.svg"
-          width={50}
-          height={50}
-          className="w-12 h-12 hover:scale-110 transition-transform"
-        />
-      </Link>
+        {/* Ícone do Github */}
+        <Link href="https://github.com/Carloscavalcante97" target="_blank">
+          <Image
+            alt="Logo Github"
+            src="/github.svg"
+            width={50}
+            height={50}
+            className="w-10 h-10 sm:w-12 sm:h-12 hover:scale-110 transition-transform"
+          />
+        </Link>
+      </div>
     </footer>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,31 +5,32 @@ import Footer from "@/components/Footer";
 
 const Home = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-950 text-white px-10">
-      <div className="flex flex-row items-center justify-between w-full max-w-6xl mx-auto ">
-     
-      <div className="w-1/2 relative bottom-30 ">
-      <Header title="Hello There!!" subtitle="
-      Sou Antônio Cavalcante, desenvolvedor Full-Stack focado em C#, Java,
-       Node.js, TypeScript, React e bancos de dados como SQL Server, MySQL
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-950 text-white px-6 sm:px-10 pb-20">
+
+      <div className="flex flex-col sm:flex-row items-center justify-between w-full max-w-6xl mx-auto ">
+      
+      <div className="w-full sm:w-1/2 relative sm:bottom-30 text-center sm:text-left">
+        <Header title="Hello There!!" subtitle="
+        Sou Antônio Cavalcante, desenvolvedor Full-Stack focado em C#, Java,
+        Node.js, TypeScript, React e bancos de dados como SQL Server, MySQL
         e PostgreSQL. Gosto de desafios que envolvem lógica e performance.
-         No tempo livre, toco baixo, jogo Dota, revisito O Senhor dos Anéis
-          e aprecio um bom café enquanto programo. Busco criar
+        No tempo livre, toco baixo, jogo Dota, revisito O Senhor dos Anéis
+        e aprecio um bom café enquanto programo. Busco criar
         soluções diferentes e bem estruturadas, sempre aprimorando meu trabalho." />
       </div>
 
-      <div className="w-1/2 flex flex-col items-center">
+      <div className="w-full sm:w-1/2 flex flex-col items-center mt-8 sm:mt-0">
         <Image
-        src="/antonio.png"
-        alt="Foto de perfil de Antonio"
-        width={456}
-        height={250}
-        className="rounded-full shadow-lg border-4 border-gray-950"
+          src="/antonio.png"
+          alt="Foto de perfil de Antonio"
+          width={250}
+          height={250}
+          className="rounded-full shadow-lg border-4 border-gray-950"
         />
         <Link href="/projects">
-        <button className="mt-6 px-6 py-2 bg-green-600 text-black rounded-lg shadow-md hover:bg-green-600 transition">
-          Meus projetos
-        </button>
+          <button className="mt-6 px-6 py-2 bg-green-600 text-black rounded-lg shadow-md hover:bg-green-700 transition">
+            Meus projetos
+          </button>
         </Link>
       </div>
       </div>
@@ -37,5 +38,4 @@ const Home = () => {
     </div>
   );
 };
-
 export default Home;
